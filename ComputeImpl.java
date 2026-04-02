6ee390c11b60a03fd60ff6c91a8f8bc944d0d60b
Ben Goering <bzgoering@icloud.com>
2026-04-02 17:40:28 -0500

    Implement remote computation methods in ComputeImpl

---
+++ ComputeImpl.java
@@ -0,0 +1,25 @@
+import java.rmi.*;
+import java.rmi.server.*;
+
+//Implimentation of interface for server
+public class ComputeImpl extends UnicastRemoteObject implements ComputeInterface
+{
+    public ComputeImpl () throws RemoteException {}
+
+    public double add(double x, double y) throws RemoteException
+    {
+        return x+y;
+    }
+    public double sub(double x, double y) throws RemoteException
+    {
+        return x-y;
+    }
+    public double multiply(double x, double y) throws RemoteException
+    {
+        return x*y;
+    }
+    public double divide(double x, double y) throws RemoteException
+    {
+        return x/y;
+    }
+}
